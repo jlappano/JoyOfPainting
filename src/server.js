57171ef681b7b7ddbf6ffd715444ad40da6b18a1
Julie lappano <julie.lappano@gmail.com>
2016-06-13 13:08:48 -0400

    updated server.js

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ var PORT = process.env.PORT || 8080
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '../dist/index.html')
+  response.sendFile('index.html')
 });
 
 app.listen(PORT, function(error) {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.info("==> 🌎  Listening on port %s.", PORT);
   }
 });
