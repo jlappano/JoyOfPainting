04a07b46deb2b652c275365118f3d1084bb9aa21
Julie lappano <julie.lappano@gmail.com>
2016-06-13 12:58:16 -0400

    reconfigured package.json, server, and webpack prod config

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
-const path = require('path')
-const express = require('express')
+var path = require('path');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, '../index.html')
-    const publicPath = express.static(path.join(__dirname, '../dist'))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '../dist/index.html')
+});
 
-    return app
+app.listen(PORT, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   }
-}
+});
--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'source-map',
 
   entry: [
-    "./src/app.js"
+    "../src/app.js"
   ],
 
   output: {
@@ -30,14 +30,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel']},
-      { test: /\.json$/, loader: 'json'},
-      { test: /\.scss$/, loaders: ["style", "css", "sass"]},
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+            { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel']},
+            { test: /\.json$/, loader: 'json'},
+            { test: /\.scss$/, loaders: ["style", "css", "sass"]},
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   }
 }
