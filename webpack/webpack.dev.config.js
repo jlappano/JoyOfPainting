69664c0ac46a668c09e9f27e3aad41ba64f790da
Julie lappano <julie.lappano@gmail.com>
2016-06-14 14:52:49 -0400

    fixed babelrc

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -13,6 +13,11 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+            'NODE_ENV': JSON.stringify('development')
+            }
+        }),
         new webpack.NoErrorsPlugin()
     ],
     module: {
