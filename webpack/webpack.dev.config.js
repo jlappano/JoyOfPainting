17a860256972035e88f304172fe7e2b09c8f810d
Julie lappano <julie.lappano@gmail.com>
2016-06-15 14:50:12 -0400

    added postinstall script

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -15,7 +15,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
-            'NODE_ENV': JSON.stringify('development')
+                'NODE_ENV': JSON.stringify('development')
             }
         }),
         new webpack.NoErrorsPlugin()
